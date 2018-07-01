7eb6e15e20e36a88df6461ab9c7ef5e0ac99a954
richardspaulm <richardspaulm@gmail.com>
2018-07-01 14:54:34 +0700

    Layout Finish

---
+++ js/mainScript.js
@@ -0,0 +1,8 @@
+var emailDiv = document.getElementById("email");
+
+//setTimeout(function(){ 	emailDiv.style.visibility = "visible"; }, 3000);
+
+function CloseEmail()
+{
+	emailDiv.style.visibility = "hidden";
+}
